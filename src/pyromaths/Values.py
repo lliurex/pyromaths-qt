de6e38177d07610a0dccf3e77ce359591ef3a83f
Jérôme Ortais <jerome.ortais@pyromaths.org>
2018-06-26 09:59:47 +0200

    Pyromaths Release

--- src/pyromaths/Values.py
+++ src/pyromaths/Values.py
@@ -62,7 +62,7 @@ else:
     def configdir():
         return join(home(), ".config", "pyromaths")
 
-VERSION = '18.6.2'
+VERSION = '18.6.3'
 COPYRIGHT_YEAR = strftime('%Y')
 COPYRIGHTS = (u'© 2006 – %s Jérôme Ortais<br/>\n' \
         u'<span style=" font-size:small;">Pyromaths est distribué sous ' \
